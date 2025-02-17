4160c2e6a4d455430e97e17a7c66a1942b5b4eb2
williamsfavour <williamsfavour012345@gmail.com>
2025-02-17 09:41:46 +0200

    Pages

--- front-end-react/src/main.jsx
+++ front-end-react/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import LandingPage from './LandingPage.jsx'
+import LogInPage from './pages/LoginPage.jsx'
+import CreateUser from './pages/CreateUserPage.jsx'
+import AdminDashBoard from './pages/AdminDashBoard.jsx'
+import DoctorDashboard from './pages/DoctorDashBoard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<LandingPage/>} />
+        <Route path='/login' element={<LogInPage/>} />
+        <Route path='/createUser' element={<CreateUser/>} />
+        <Route path='/admin' element={<AdminDashBoard/>} />
+        <Route path='/doctorDashboard' element={<DoctorDashboard/>} />
+
+        {/* <Route path='/' element={<LandingPage/>} /> */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
