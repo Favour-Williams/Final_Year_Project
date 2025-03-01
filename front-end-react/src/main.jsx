ee2d3c33564416f554416e8b1cf2f7b0801edb65
Favour-Williams <122178439+Favour-Williams@users.noreply.github.com>
2025-03-01 18:04:11 +0200

    Add files via upload

--- front-end-react/src/main.jsx
+++ front-end-react/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LandingPage from './LandingPage.jsx'
 import LogInPage from './pages/LoginPage.jsx'
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 import CreateUser from './pages/CreateUserPage.jsx'
 import AdminDashBoard from './pages/AdminDashBoard.jsx'
 import DoctorDashboard from './pages/DoctorDashBoard.jsx'
@@ -20,6 +22,8 @@ createRoot(document.getElementById('root')).render(
 
         <Route path='/' element={<LandingPage/>} />
         <Route path='/login' element={<LogInPage/>} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
         
         <Route path='/admin' element={<AdminDashBoard/>} />
         <Route path='/admin/createUser' element={<CreateUser/>} />
