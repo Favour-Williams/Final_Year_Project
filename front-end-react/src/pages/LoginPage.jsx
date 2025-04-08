41b5692291fecc63e8135b45eba431bc2a0ae85e
Favour-Williams <122178439+Favour-Williams@users.noreply.github.com>
2025-04-08 14:31:46 +0200

    Add files via upload

--- front-end-react/src/pages/LoginPage.jsx
+++ front-end-react/src/pages/LoginPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Input from '../components/Inputs';
 import Button from '../components/Button';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom'; 
